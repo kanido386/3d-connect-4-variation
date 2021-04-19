b52788c08cf2dd8b9767c5d3a25f42ea577bec0a
kanido386 <kanido386@gmail.com>
2021-04-19 21:13:01 +0800

    mcts: add basic structure

--- mcts.py
+++ mcts.py
@@ -12,3 +12,36 @@ class TreeNode():
     self.visit = 0
     self.score = 0
     self.children = {}
+
+class MCTS():
+  
+  # search for the best move in the current position
+  def search(self, initial_state):
+    self.root = TreeNode(initial_state, None)
+
+    for iteration in range(300):
+      node = self.select(self.root)     # select a node (selection phase)
+      score = self.rollout(node.board)  # score current node (simulation phase)
+      self.backpropagate(node, score)
+    
+    return get_best_move(self.root, 0)
+
+
+  # select most promising node
+  def select(self, node):
+    pass
+
+
+  # simulate the game via random moves until reaching the end of the game
+  def rollout(self, board):
+    pass
+
+
+  # backpropagate the number of visits and score up to the root node
+  def backpropagate(self, node, score):
+    pass
+
+
+  # select the best node based on UCB1 formula
+  def get_best_move(self, node, exploration_constant):
+    pass
